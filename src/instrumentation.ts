e292f627983f01458005ceb0f54c443fe62a0d49
Scott Patrick <scott.ipatrick@edpsychconnect.app>
2025-11-29 18:36:30 +0000

    Fix merge conflicts in instrumentation.ts and update lockfile

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-/**
- * @copyright EdPsych Connect Limited 2025
- * @license Proprietary - All Rights Reserved
- * 
- * CONFIDENTIAL: This software contains proprietary algorithms and trade secrets.
- * Unauthorized copying, modification, distribution, or use is strictly prohibited.
- */
-=======
 import { logger } from "@/lib/logger";
 /**
  * @copyright EdPsych Connect Limited 2025
@@ -20,14 +11,4 @@ export async function register() {
   if (process.env.NEXT_RUNTIME === 'nodejs') {
     await import('../sentry.server.config');
   }
->>>>>>> bb81e2ee63fdae53ae03162751986938e3dba36b
-
-export async function register() {
-  // if (process.env.NEXT_RUNTIME === 'nodejs') {
-  //   await import('../sentry.server.config');
-  // }
-
-  // if (process.env.NEXT_RUNTIME === 'edge') {
-  //   await import('../sentry.edge.config');
-  // }
 }
